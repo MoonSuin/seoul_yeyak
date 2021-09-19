67e84f5ffc59f48cc52893b97672d6ba47be31ce
MoonSuin <84334574+MoonSuin@users.noreply.github.com>
2021-09-19 19:12:20 +0900

    create

---
+++ models/cul_model.py
@@ -0,0 +1,23 @@
+class Culture:
+    def __init__(self):
+        self.svcid
+        self.minclassnm
+        self.svcstatnm
+        self.svcnm
+        self.payatnm
+        self.placenm
+        self.usetgtinfo
+        self.imgurl
+        self.svcurl
+        self.x
+        self.y
+        self.rcptbgndt
+        self.rcptenddt
+        self.areanm
+
+class CulService:
+    def __init__(self):
+        base_url = 'http://openAPI.seoul.go.kr:8088/'
+        api_key='4c4c556a567375693633667243655a'
+        url=base_url+api_key+'/xml/ListPublicReservationCulture/1/200/'
+
